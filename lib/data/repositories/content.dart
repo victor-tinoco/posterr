f647ccbeb567a0399bdbc19bc9ef0e7f8e31a558
Victor Tinoco <victormartinstinoco@live.com>
2022-07-27 00:29:03 -0300

    Mock some default content on mocked repo

--- lib/data/repositories/content.dart
+++ lib/data/repositories/content.dart
@@ -1,7 +1,42 @@
 import 'package:posterr/domain/domain.dart';
 
 class ContentRepositoryMock implements ContentRepository {
-  final _cache = <Content>[];
+  final _cache = <Content>[
+    Content.quotePost(
+      author: User(username: 'victor-tinoco', joinedAt: DateTime(2020, 3, 2)),
+      message: 'I totally agree.',
+      post: Post(
+        author: User(username: 'alesandro123', joinedAt: DateTime(2020, 3, 17)),
+        message: "I really didn't like it. Backing to the neighbor blue social media.",
+        postedAt: DateTime(2022, 7, 27),
+      ),
+      postedAt: DateTime(2022, 7, 27),
+    ),
+    Content.post(
+      author: User(username: 'alesandro123', joinedAt: DateTime(2020, 3, 17)),
+      message: "I really didn't like it. Backing to the neighbor blue social media.",
+      postedAt: DateTime(2022, 7, 27),
+    ),
+    Content.repost(
+      author: User(username: 'victor-tinoco', joinedAt: DateTime(2020, 3, 2)),
+      originalPost: Post(
+        author: User(username: 'jordanp2', joinedAt: DateTime(2020, 7, 3)),
+        message: "Just sharing some content here. Amazing new social media! Congrats Posterr's team!",
+        postedAt: DateTime(2022, 7, 27),
+      ),
+      postedAt: DateTime(2022, 7, 27),
+    ),
+    Content.post(
+      author: User(username: 'alanis-c', joinedAt: DateTime(2020, 3, 17)),
+      message: "It's really good. Damn twitter!",
+      postedAt: DateTime(2022, 7, 27),
+    ),
+    Content.post(
+      author: User(username: 'jordanp2', joinedAt: DateTime(2020, 7, 3)),
+      message: "Just sharing some content here. Amazing new social media! Congrats Posterr's team!",
+      postedAt: DateTime(2022, 7, 27),
+    ),
+  ];
 
   @override
   Future<Result<List<Content>>> getContents({
