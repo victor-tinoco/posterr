1cf5d2b1e357b7a06eb8c6a7c4ac2404b05c4e32
Victor Tinoco <victormartinstinoco@live.com>
2022-07-27 02:04:55 -0300

    Add timeline for profile page

--- lib/ui/pages/profile/profile_page.dart
+++ lib/ui/pages/profile/profile_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+import 'package:posterr/data/repositories/auth.dart';
+import 'package:posterr/data/repositories/content.dart';
+import 'package:posterr/domain/domain.dart';
+import 'package:posterr/ui/widgets/timeline/timeline.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -10,11 +16,89 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  final contentRepository = ContentRepositoryMock();
+  final authRepository = AuthRepositoryMock();
+
+  late final getLoggedUserContent = GetLoggedUserContent(contentRepository: contentRepository, authRepository: authRepository);
+  late final sharePost = SharePost(contentRepository: contentRepository, authRepository: authRepository, getLoggedUserContent: GetLoggedUserContent(contentRepository: contentRepository, authRepository: authRepository));
+
+  final sharePostTextController = TextEditingController();
+
+  @override
+  void dispose() {
+    sharePostTextController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+    return BlocProvider(
+      create: (context) => TimelineBloc(getContent: getLoggedUserContent, sharePost: sharePost) //
+        ..add(const TimelineEvent.contentsFetched()),
+      child: Builder(
+        builder: (context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Profile'),
+            ),
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: ValueListenableBuilder<User?>(
+                    valueListenable: authRepository.loggedUser,
+                    builder: (context, user, child) {
+                      final joinedAt = user?.joinedAt;
+
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text('Username', style: TextStyle(color: Colors.grey)),
+                          Text(user?.username ?? ''),
+                          const SizedBox(height: 8.0),
+                          const Text('Joined at', style: TextStyle(color: Colors.grey)),
+                          Text(joinedAt != null ? DateFormat.yMMMd().format(joinedAt) : ''),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                const Divider(thickness: 1.0, height: 1.0),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: TextField(
+                                controller: sharePostTextController,
+                              ),
+                            ),
+                            const SizedBox(width: 8.0),
+                            IconButton(
+                              icon: const Icon(Icons.send),
+                              onPressed: () {
+                                final bloc = context.read<TimelineBloc>();
+                                final event = TimelineEvent.postShared(sharePostTextController.text);
+                                bloc.add(event);
+                                sharePostTextController.clear();
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Expanded(child: Timeline())
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
